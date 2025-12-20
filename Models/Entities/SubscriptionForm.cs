dbb35e7b3dabcfc3d3a628541b5537f6fad0264f
Sky1sBloo <ultimate8gaming@gmail.com>
2025-12-20 19:02:36 +0800

    Separated subscription create and get views

--- Models/Entities/SubscriptionForm.cs
+++ Models/Entities/SubscriptionForm.cs
@@ -1,10 +1,12 @@
 using FinanceApp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinanceApp.Models.Entities
 {
     public class SubscriptionForm
     {
-        public string? Name { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public required string Name { get; set; }
         public TransactionCategory Category { get; set; }
         public int Amount { get; set; }
         public FrequencyType Frequency { get; set; }
--- Models/Entities/SubscriptionIndexViewModel.cs
+++ Models/Entities/SubscriptionIndexViewModel.cs
@@ -2,7 +2,6 @@ namespace FinanceApp.Models.Entities
 {
     public class SubscriptionIndexViewModel 
     {
-        public required SubscriptionForm Form { get; set; }
         public required List<Subscription> Subscriptions { get; set; }
     }
 }
