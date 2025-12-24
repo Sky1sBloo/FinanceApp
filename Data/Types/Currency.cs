2782ebb94a05135be4f4030ada3c4f65c7fb483e
Sky1sBloo <ultimate8gaming@gmail.com>
2025-12-24 20:23:27 +0800

    Added currency formatting on higher values

--- Data/Types/Currency.cs
+++ Data/Types/Currency.cs
@@ -9,7 +9,26 @@ namespace FinanceApp.Data.Types
         }
         public override string ToString()
         {
+            if (Amount > 1000)
+            {
+                return "Php " + (Amount / 1000).ToString("0.##") + "k";
+            } else if (Amount > 1000000)
+            {
+                return "Php " + (Amount / 1000000).ToString("0.##") + "M";
+            }
             return "Php " + Amount.ToString();
+        }
+
+        public static string Format(decimal amount)
+        {
+            if (amount > 1000)
+            {
+                return "Php " + (amount / 1000).ToString("0.##") + "k";
+            } else if (amount > 1000000)
+            {
+                return "Php " + (amount / 1000000).ToString("0.##") + "M";
+            }
+            return "Php " + amount.ToString();
         }
     }
 }
